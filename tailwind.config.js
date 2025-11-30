801b88d65804e77704fa5f208ae01063f1607882
Deny2503 <bilotserkovskiydimayt@gmail.com>
2025-11-30 21:45:45 +0300

    Create new design, add style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,14 @@
 ﻿/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./Views/**/*.cshtml",
-    "./Pages/**/*.cshtml",
-    "./wwwroot/**/*.js"
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        "./Views/**/*.cshtml",
+        "./Pages/**/*.cshtml",
+        "./wwwroot/css/**/*.css",
+        "./Controllers/**/*.cs",
+        "./Models/**/*.cs"
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
 }
