2ad869c7f064d9e54d2735281cc692790a5d3b57
CementD <gavrik0612@gmail.com>
2025-11-30 19:09:17 +0100

    Controllers, PasswordService

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,21 +1,26 @@
-﻿using WebApplication16.Data;
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication16.Data;
 using WebApplication16.Data.Models;
-
-using Microsoft.AspNetCore.Mvc;
+using WebApplication16.Services;
 
 namespace WebApplication16.Controllers
 {
     public class AuthController : Controller
     {
         private readonly AppDbContext _context;
-        public AuthController(AppDbContext context)
+        private readonly IPasswordService _passwordService;
+
+        public AuthController(AppDbContext context, IPasswordService passwordService)
         {
             _context = context;
+            _passwordService = passwordService;
         }
+
         public IActionResult Register()
         {
             return View();
         }
+
         [HttpPost]
         public async Task<IActionResult> Register(string username, string email, string password)
         {
@@ -29,24 +34,26 @@ namespace WebApplication16.Controllers
             {
                 Username = username,
                 Email = email,
-                HashPassword = password
+                HashPassword = _passwordService.HashPassword(password)
             };
+
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
 
             HttpContext.Session.SetInt32("UserId", user.Id);
-
-            return RedirectToAction("Login");
+            return RedirectToAction("Index", "Document");
         }
+
         public IActionResult Login()
         {
             return View();
         }
+
         [HttpPost]
         public IActionResult Login(string email, string password)
         {
-            var user = _context.Users.FirstOrDefault(u => u.Email == email && u.HashPassword == password);
-            if (user == null)
+            var user = _context.Users.FirstOrDefault(u => u.Email == email);
+            if (user == null || !_passwordService.VerifyPassword(user.HashPassword, password))
             {
                 ModelState.AddModelError("", "Invalid credentials");
                 return View();
@@ -55,6 +62,7 @@ namespace WebApplication16.Controllers
             HttpContext.Session.SetInt32("UserId", user.Id);
             return RedirectToAction("Index", "Document");
         }
+
         [HttpPost]
         public IActionResult Logout()
         {
